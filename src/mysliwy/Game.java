48aac414570cccaa12693d8fb8d80c42c3cdee7b
Pawel Pogorzelski <01130013@pw.edu.pl>
2021-01-03 19:30:00 +0100

    level_Data - wczytywanie z pliku konfiguracyjnego mapy

--- src/mysliwy/Game.java
+++ src/mysliwy/Game.java
@@ -1,18 +1,41 @@
 package mysliwy;
 
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
 public class Game extends JPanel implements ActionListener {
 
+    MapConfig temporary
     private Dimension m;
+    private final Font smallFont = new Font("Arial", Font.BOLD, 14);
+    private boolean wGrze = false;
+    private boolean smierc = false;
 
 
+    private final int BLOCK_SIZE = 24;
+    private final int N_BLOCKS = 15;
+    private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
+    private final int MAX_GHOSTS = 12;
+    private final int PACMAN_SPEED = 6;
 
+    private int N_GHOSTS = 6;
+    private int lives, score;
+    private int[] dx, dy;
+    private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
 
+    private Image heart, ghost;
+    private Image up, down, left, right;
 
+    private int pacman_x, pacman_y, pacmand_x, pacmand_y;
+    private int req_dx, req_dy;
+
+    private final short levelData[] = // tu wjeżdza mapka z Map Parsing
 
     public void actionPerformed(ActionEvent e) {
     //code that reacts to the action...
--- src/mysliwy/Main.java
+++ src/mysliwy/Main.java
@@ -3,7 +3,6 @@ package mysliwy;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Paweł!");
         mysliwy.MapParsing config = new mysliwy.MapParsing();
         config.LevelRead("Config1.txt");
     }
